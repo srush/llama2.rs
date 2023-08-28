45c48ff85dddfdede92cc8ad8bcb011eb7965588
Rachit Singh <rachitsingh@outlook.com>
2023-08-27 21:56:38 -0400

    added group_* and M1 support

--- build.rs
+++ build.rs
@@ -1,31 +1,65 @@
+use std::env;
+
 fn main() {
     // note: Cargo features are just binary flags, so this decides a more clear cfg key/value pair
-    if cfg!(feature="7B") {
-        println!("cargo:rustc-cfg=model_size=\"7B\"");
+    let model_size = if cfg!(feature="7B") {
+        "7B"
     } else if cfg!(feature="13B") {
-        println!("cargo:rustc-cfg=model_size=\"13B\"");
+        "13B"
     } else  if cfg!(feature="70B") {
-        println!("cargo:rustc-cfg=model_size=\"70B\"");
+        "70B"
     } else {
         panic!("Must set one of 7B, 13B or 70B features to pick a model size");
-    }
+    };
+    println!("cargo:rustc-cfg=model_size=\"{}\"", model_size);
+
+    // get group size
+    let group_size = if cfg!(feature="group_32") {
+        32
+    } else if cfg!(feature="group_64") {
+        64
+    } else if cfg!(feature="group_128") {
+        128
+    } else {
+        // ok if there's no quantization
+        0
+    };
 
     // set quantization level
     if cfg!(feature="quantized") {
-        println!("cargo:rustc-cfg=quant=\"Q_4_128\"");
+        if group_size == 0 {
+            panic!("Must set one of group_32, group_64 or group_128 features to use quantization");
+        }
+        println!("cargo:rustc-cfg=group_size=\"{}\"", group_size);
+        println!("cargo:rustc-cfg=quant=\"Q_4\"");
     }
     else {
         println!("cargo:rustc-cfg=quant=\"no\"");
     }
 
     // set target-feature based on AVX512/AVX2 desired support
-    if cfg!(feature="avx512") {
+    let avx512 = if cfg!(feature="avx512") {
         if !std::is_x86_feature_detected!("avx512f") {
-            panic!("AVX512 is not supported on this machine");
+            panic!("AVX512 is not supported on this machine, but the avx512 feature was requested");
         }
-        println!("cargo:rustc-env=RUSTFLAGS=-C target-feature=+avx,+avx2,+fma,+sse3,+avx512f target-cpu=native");
+        ",+avx512f "
     }
-    else if std::is_x86_feature_detected!("avx2") {
-        println!("cargo:rustc-env=RUSTFLAGS=-C target-feature=+avx,+avx2,+fma,+sse3 target-cpu=native");
+    else {
+        ""
+    };
+
+    let mut rustflags = [
+        "-C target-cpu=native",
+        "-C link-args=-Wl,-zstack-size=419430400"
+    ].map(String::from).to_vec();
+
+    match env::var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
+        "x86_64" => {
+            rustflags.push(format!("-C target-feature=+avx,+avx2,+fma,+sse3{}", avx512));
+        },
+        // on Mac M* devices, SIMD support is via NEON, not AVX (and is enabled via target-cpu=native by default)
+        // leave this here since we'll need explicit Accelerate linking for BLAS
+        "aarch64" | _ => {},
     }
+    println!("cargo:rustc-env=RUSTFLAGS={}", rustflags.join(" "));
 }
